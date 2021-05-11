a2ece43c7db50834035e890377847fca9ce91361
dougpagani <redstonefreedom@gmail.com>
2021-05-11 17:13:48 -0400

    Add a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+################################################################################
+
+# grab toggldesktop since it has some useful code; it is ignored so we dont
+# deal with the git-in-git-problem.
+git clone git@github.com:toggl-open-source/toggldesktop.git
