7f3bb5f36ae829ace876510e1bff8cb1e081a7af
dougpagani <redstonefreedom@gmail.com>
2021-05-12 16:45:52 -0400

    Add first piece of source -- logging the info

---
+++ src/log-active-window.js
@@ -0,0 +1,74 @@
+/* this is our snapshots/event stream of computer activity */
+const activeWindow = require('active-win');
+
+async function main() {
+  const rawWindowInfo = await getActiveWindowInfo()
+  const logEntry = buildLogEntryFromRawInfo(rawWindowInfo)
+  // appendLogEntryToLogFile()
+  console.log(logEntry)
+}
+
+async function getActiveWindowInfo() {
+  return await activeWindow()
+}
+
+function getTimeStamp() {
+  return new Date();
+  // e.g. 2021-05-12T20:37:51.243Z
+}
+
+// TODO: build with ts type definitions
+function buildLogEntryFromRawInfo(rawActiveWindowInfo) {
+
+  // TODO: blacklist values instead; copy-over and start modifiying 
+  // ... that way, you dont lose any special values like for example chrome has
+  const activeWindowLogEntry = {
+    timestamp: getTimeStamp(),
+    title: rawActiveWindowInfo.title,
+    app: rawActiveWindowInfo.owner.name,
+    url: rawActiveWindowInfo.url,
+  }
+  return activeWindowLogEntry
+
+  // FROM THIS:
+	/*
+	{
+		title: 'Unicorns - Google Search',
+		id: 5762,
+		bounds: {
+			x: 0,
+			y: 0,
+			height: 900,
+			width: 1440
+		},
+		owner: {
+			name: 'Google Chrome',
+			processId: 310,
+			bundleId: 'com.google.Chrome',
+			path: '/Applications/Google Chrome.app'
+		},
+		url: 'https://sindresorhus.com/unicorn',
+		memoryUsage: 11015432
+	}
+	*/
+  // TO THIS:
+	/*
+	{
+                timestamp: SOME_PLEASING_FORMAT,
+		title: 'Unicorns - Google Search',
+                app: 'Google Chrome',
+		url: 'https://sindresorhus.com/unicorn',
+	}
+	*/
+}
+
+if (require.main === module) {
+  main()
+}
+
+
+// For a fancier invocation, build from here:
+// https://gist.github.com/benjamingr/0237932cee84712951a2
+process.on('unhandledRejection', (reason) => {
+  console.log("\x1b[38;5;1m UPR: ", reason)
+});
