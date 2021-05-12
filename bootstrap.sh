766e89caf7a873146599e431cc7e11af1ed5c9e1
dougpagani <redstonefreedom@gmail.com>
2021-05-12 15:53:50 -0400

    Add check for jq
    It is not necessary, but highly suggested as per a recent addition to
    docs. You could theoretically build queries some other way.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,32 @@ main() {
     # grab toggldesktop since it has some useful code; it is ignored so we dont
     # deal with the git-in-git-problem.
     git clone git@github.com:toggl-open-source/toggldesktop.git || :
-    
+
+    preflight-helper-utility-check jq
+
     modify-crontab
 }
+
+printred() {
+    c_grey="\x1b[38;5;1m"
+    nc="\033[0m"
+    printf "${c_grey}%s${nc}\n" "$*" >&2
+}
+
+die() {
+    printred "$@" >&2
+    exit 1
+}
+
+preflight-helper-utility-check() {
+    for dep in "$@"; do
+        if ! (type $dep > /dev/null); then
+            die "missing dependency: $dep; please add install to this script ${BASH_SOURCE[0]}"
+        fi
+    done
+}
+
+
 modify-crontab() {
     crontabLine=$(prepare-crontab-line)
     # scriptPath="$absolutePathToScript" # HACK: comes from dynamic scope
